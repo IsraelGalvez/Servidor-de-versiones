7187340dac17f274f1dba1259ee2460f9d341449
sebastianisaacvegarobles <sebastianisaacvegarobles@192.168.1.66>
2021-02-10 14:43:34 -0700

    Escribir imprensión en consola en el archivo Holamundo.java

--- src/holamundo/Holamundo.java
+++ src/holamundo/Holamundo.java
@@ -1,22 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package holamundo;
 
 /**
  *
  * @author nakam
+ * @author Sebastianisaacv
  */
 public class Holamundo {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Holamundo");
         System.out.println("HolaMundo1.2");
+        
+        System.out.println("Sebastián escribio en este archivo");
     }
     
 }
